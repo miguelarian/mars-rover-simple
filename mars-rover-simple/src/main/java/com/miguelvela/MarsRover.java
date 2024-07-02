62785e5decbee6d43a1c00795c8498e10e1d0ef7
miguelarian <miguelvr88@gmail.com>
2024-07-02 18:41:39 +0200

    add right rotation behaviour

--- mars-rover-simple/src/main/java/com/miguelvela/MarsRover.java
+++ mars-rover-simple/src/main/java/com/miguelvela/MarsRover.java
@@ -2,9 +2,42 @@ package com.miguelvela;
 
 public class MarsRover
 {
-    private static final String INITIAL_POSITION = "0:0:N";
+    private static final String INITIAL_POSITION = "0:0";
+
+    private char direction = 'N';
+
 
     public String Execute(String command) {
-        return INITIAL_POSITION;
+
+        for (char action : command.toCharArray()) {
+
+            if (action == 'R') {
+                this.direction = rotateRight(this.direction, action);
+            }
+
+            if (action == 'L') {
+                return INITIAL_POSITION + ":W";
+            }
+        }
+
+        return INITIAL_POSITION + ":" + this.direction;
+    }
+
+    private char rotateRight(char currentPosition, char direction) throws IllegalArgumentException {
+        if (currentPosition == 'N' && direction == 'R') {
+            return 'E';
+        }
+        else if(currentPosition == 'E' && direction == 'R') {
+            return 'S';
+        }
+        else if(currentPosition == 'S' && direction == 'R') {
+            return 'W';
+        }
+        else if(currentPosition == 'W' && direction == 'R') {
+            return 'N';
+        }
+        else {
+            throw new IllegalArgumentException("Invalid direction");
+        }
     }
 }
--- mars-rover-simple/src/test/java/com/miguelvela/MarsRoverTests.java
+++ mars-rover-simple/src/test/java/com/miguelvela/MarsRoverTests.java
@@ -13,4 +13,38 @@ public class MarsRoverTests {
         assertEquals("0:0:N", result);
     }
 
+    @Test
+    public void Execute_FromInitialToRight_ReturnsEast() {
+        MarsRover rover = new MarsRover();
+        String result = rover.Execute("R");
+        assertEquals("0:0:E", result);
+    }
+
+    @Test
+    public void Execute_FromInitialToLeft_ReturnsWest() {
+        MarsRover rover = new MarsRover();
+        String result = rover.Execute("L");
+        assertEquals("0:0:W", result);
+    }
+
+    @Test
+    public void Execute_FromInitialRotate180Right_ReturnsSouth() {
+        MarsRover rover = new MarsRover();
+        String result = rover.Execute("RR");
+        assertEquals("0:0:S", result);
+    }
+
+    @Test
+    public void Execute_FromInitialRotate0Right_ReturnsWest() {
+        MarsRover rover = new MarsRover();
+        String result = rover.Execute("RRR");
+        assertEquals("0:0:W", result);
+    }
+
+    @Test
+    public void Execute_FromInitialRotate360Right_ReturnsInitialDirection() {
+        MarsRover rover = new MarsRover();
+        String result = rover.Execute("RRRR");
+        assertEquals("0:0:N", result);
+    }
 }
