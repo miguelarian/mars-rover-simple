6b2d3708c4826d9a7953e7669ae2607e37321ad1
miguelarian <miguelvr88@gmail.com>
2024-07-03 16:51:48 +0200

    refactor: extract constants for directions and enum for actions

--- mars-rover-simple/src/main/java/com/miguelvela/MarsRover.java
+++ mars-rover-simple/src/main/java/com/miguelvela/MarsRover.java
@@ -6,6 +6,32 @@ public class MarsRover
     private final int INITIAL_Y_COORDINATE = 0;
     private final int INITIAL_DIRECTION = 'N';
 
+    private final char NORTH = 'N';
+    private final char SOUTH = 'S';
+    private final char EAST = 'E';
+    private final char WEST = 'W';
+
+    private enum Action {
+        ROTATE_RIGHT('R'),
+        ROTATE_LEFT('L'),
+        MOVE('M');
+
+        private char value;
+
+        Action(char action) {
+            this.value = action;
+        }
+
+        public static Action fromValue(char value) {
+            for (Action action : Action.values()) {
+                if (action.value == value) {
+                    return action;
+                }
+            }
+            throw new IllegalArgumentException("Invalid action");
+        }
+    }
+
     private int XCoordinate;
     private int YCoordinate;
     private char direction;
@@ -24,11 +50,12 @@ public class MarsRover
 
     public String Execute(String command) {
 
-        for (char action : command.toCharArray()) {
+        for (char inputAction : command.toCharArray()) {
+            Action action = Action.fromValue(inputAction);
             switch (action) {
-                case 'R' -> this.direction = rotateRight(this.direction);
-                case 'L' -> this.direction = rotateLeft(this.direction);
-                case 'M' -> move();
+                case ROTATE_RIGHT -> this.direction = rotateRight(this.direction);
+                case ROTATE_LEFT -> this.direction = rotateLeft(this.direction);
+                case MOVE -> move();
                 default -> throw new IllegalArgumentException("Invalid action");
             }
         }
@@ -36,35 +63,35 @@ public class MarsRover
     }
 
     private void move() {
-        if (this.direction == 'N') {
+        if (this.direction == NORTH) {
             this.YCoordinate++;
         }
 
-        if (this.direction == 'E') {
+        if (this.direction == EAST) {
             this.XCoordinate++;
         }
 
-        if (this.direction == 'S') {
+        if (this.direction == SOUTH) {
             this.YCoordinate--;
         }
 
-        if (this.direction == 'W') {
+        if (this.direction == WEST) {
             this.XCoordinate--;
         }
     }
 
     private char rotateLeft(char currentPosition) {
-        if (currentPosition == 'N') {
-            return 'W';
+        if (currentPosition == NORTH) {
+            return WEST;
         }
-        else if(currentPosition == 'W') {
-            return 'S';
+        else if(currentPosition == WEST) {
+            return SOUTH;
         }
-        else if(currentPosition == 'S') {
-            return 'E';
+        else if(currentPosition == SOUTH) {
+            return EAST;
         }
-        else if(currentPosition == 'E') {
-            return 'N';
+        else if(currentPosition == EAST) {
+            return NORTH;
         }
         else {
             throw new IllegalArgumentException("Invalid rotation");
@@ -72,17 +99,17 @@ public class MarsRover
     }
 
     private char rotateRight(char currentPosition) throws IllegalArgumentException {
-        if (currentPosition == 'N') {
-            return 'E';
+        if (currentPosition == NORTH) {
+            return EAST;
         }
-        else if(currentPosition == 'E') {
-            return 'S';
+        else if(currentPosition == EAST) {
+            return SOUTH;
         }
-        else if(currentPosition == 'S') {
-            return 'W';
+        else if(currentPosition == SOUTH) {
+            return WEST;
         }
-        else if(currentPosition == 'W') {
-            return 'N';
+        else if(currentPosition == WEST) {
+            return NORTH;
         }
         else {
             throw new IllegalArgumentException("Invalid rotation");
